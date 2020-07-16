61aa2c71d65f58c43ff15cd7fec362dbd5c20a22
exory3 <bogdan444254@gmail.com>
2020-07-16 19:49:35 +0300

    init commit

---
+++ demo/demo.js
@@ -0,0 +1,31 @@
+fetch("https://api.unsplash.com/photos/?client_id=s1tEk9HM7KpHHAirgZalHUGNra5LML9DRgXZsZYVATk")
+	.then(response => response.json())
+	.then(json => {
+		let picturesList = json.map(array => array.urls.full);
+		for (let i = 0; i < picturesList.length; i++) {
+			$(".demo").append('<a href="#" class="item"></a>');
+		}
+
+
+		$(".demo").diamonds({
+			size: 200,
+			gap: 2,
+			hideIncompleteRow: false, // default: false
+			autoRedraw: true, // default: true
+			itemSelector: ".item"
+		});
+
+		let items = $("a");
+		let lightbox = $(".lightbox");
+		let lightboxImage = $(".lightboxImage");
+		lightboxImage.on("click", () => lightbox.attr('style', 'display: none'));
+
+		for (let i = 0; i < items.length; i++) {
+			items[i].style.backgroundImage = `url(${picturesList[i]})`;
+			console.log(items.eq(i));
+			items.eq(i).on("click", () => {
+				lightboxImage.attr("src", picturesList[i]);
+				lightbox.attr('style', 'display: block');
+			});
+		};
+	})	
